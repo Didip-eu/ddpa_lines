d1b06e48c89219da1c339b3eb64984c8d9ccb571
Anguelos Nicolaou <anguelos.nicolaou@gmail.com>
2022-04-21 00:30:43 +0200

    first tormenting

--- misc/script_recognition/README.md
+++ misc/script_recognition/README.md
@@ -4,10 +4,18 @@
 ```bash
 mkdir -p data
 (cd data; wget http://clamm.irht.cnrs.fr/wp-content/uploads/ICDAR2017_CLaMM_task1_task3.zip; unzip ICDAR2017_CLaMM_task1_task3.zip)
+# removing non existing file that crashes dataset
+cat data/ICDAR2017_CLaMM_task1_task3/@ICDAR2017_CLaMM_task1_task3.csv | grep -v 'Task_1_3;IRHT_P_009783.tif;5;11' > /tmp/gt.csv
+cp /tmp/gt.csv data/ICDAR2017_CLaMM_task1_task3/@ICDAR2017_CLaMM_task1_task3.csv
 ```
 
 ### Download validation-set
 ```bash
 mkdir -p data
 (cd data; wget http://clamm.irht.cnrs.fr/wp-content/uploads/ICDAR2017_CLaMM_task1_task3.zip; unzip ICDAR2017_CLaMM_task1_task3.zip)
+```
+
+### Train Model
+```bash
+PYTHONPATH="./src" python3 ./bin/train.py 
 ```
--- misc/script_recognition/src/clamm_dataset.py
+++ misc/script_recognition/src/clamm_dataset.py
@@ -6,7 +6,7 @@ import tormentor
 import sys
 
 class ClammDataset(object):
-    def __init__(self, img_root, gt_fname, script_not_date, crop_to_size=512):
+    def __init__(self, img_root, gt_fname, script_not_date, crop_to_size=512, ):
         gt_tbl = [line.split(";") for line in open(gt_fname, "r").read().strip().split("\n")[1:]]
         if script_not_date:
             self.gt = [(f"{img_root}/{row[1]}", int(row[2])-1) for row in gt_tbl]
@@ -26,6 +26,46 @@ class ClammDataset(object):
     def __len__(self):
         return len(self.gt)
 
+    class2scripts = {0:"caroline",1:"cursiva",2:"half-uncial", 3:"humanistic",4:"humanistic cursive",
+                     5:"hybrida", 6:"praegothica", 7:"semihybrida", 8:"semitextualis", 9:"southern_textualis",
+                     10:"textualis", 11: "uncial"}
+    class2year_ranges = {0:(0,1000),
+    1:(1001,1101),
+    2:(1101,1201),
+    3:(1201,1251),
+    4:(1251,1301),
+    5:(1301,1351),
+    6:(1351,1401),
+    7:(1401,1426),
+    8:(1426, 1451),
+    9:(1451,1476),
+    10:(1476,1501),
+    11:(1501,1526),
+    12:(1526, 1551),
+    13:(1551,1576),
+    14:(1576,1601)}
+
+
+class ICDAR2019Script(object):
+    def __init__(self, img_root, gt_fname, script_not_date=True, crop_to_size=512):
+        gt_tbl = [line.split(",") for line in open(gt_fname, "r").read().strip().split("\n")[1:]]
+        if script_not_date:
+            script2class = {v: k for k, v in ClammDataset.class2scripts.items()}
+            self.gt = [(f"{img_root}/{row[1]}", script2class[row[1]]) for row in gt_tbl]
+            # self.gt = [(row[1], int(row[2])) for row in gt_tbl]
+        else:
+            raise NotImplementedError
+        self.input_transform = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.RandomCrop(crop_to_size, pad_if_needed=True)
+        ])
+
+    def __getitem__(self, n):
+        img = Image.open(self.gt[n][0]).convert("RGB")
+        return self.input_transform(img), torch.tensor(self.gt[n][1])
+
+    def __len__(self):
+        return len(self.gt)
 
 if __name__ == "__main__":
     ds = ClammDataset(img_root="./data/ICDAR2017_CLaMM_task1_task3/", script_not_date = True, gt_fname="data/ICDAR2017_CLaMM_task1_task3/@ICDAR2017_CLaMM_task1_task3.csv")
