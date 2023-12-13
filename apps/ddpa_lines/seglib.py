f7a9dd6fef381600291c7b6f32e87471b6b2b836
Nicolas Renet <nprenet@gmail.com>
2023-12-13 14:15:29 +0100

    Line app evaluation code roughed out.

---
+++ apps/ddpa_lines/seglib.py
@@ -0,0 +1,200 @@
+from kraken import blla
+from kraken.lib import vgsl
+from PIL import Image
+from pathlib import Path
+import torchvision
+
+#warning.warn(_BETA_TRANSFORMS_WARNING)
+torchvision.disable_beta_transforms_warning()
+
+from torchvision.transforms import v2
+from torch import Tensor
+
+import numpy as np
+import cv2
+import collections
+from typing import Tuple
+import itertools
+import torch
+
+
+
+def line_segment(img: Image.Image, model_path: str):
+    r"""
+    Args:
+        img (Image.Image): input image
+        model_path (str): model location
+
+    Output:
+        tuple: a pair with the maximum value of the line labels and the polygons rendered as an image.
+    """
+
+    if not Path( model_path ).exists():
+        raise FileNotFoundError("Cound not find model file", model_path)
+    # you want to load the model once (loaded model as a parameter)
+    return dict_to_polygons( blla.segment( img, model=vgsl.TorchVGSLModel.load_model( model_path )), img )
+
+
+# get IoU table given 2 polyg. images + binary mask + (optional: numb)
+
+# loading data and calling thisn one
+
+def get_confusion_matrix( img: Image.Image, segmentation_dict_gt, segmentation_dict_pred ) -> np.ndarray:
+    """
+    (OBSOLETE) Compute a confusion matrix for segmentation output.
+
+    Args:
+        img (Image.Image): input image.
+        segmentation_dict_gt (dict): the GT dictionary describing the lines (see above)
+        segmentation_dict_pred (dict): the inferred dictionary describing the lines, as given by the Kraken library call
+
+    Output:
+        np.ndarray: a matrix containing I/U scores for all pairs (GT polygon pi, predicted polygon pj)
+
+    """
+    lbl_count_gt, polygon_img_gt = dict_to_polygons( segmentation_dict_gt, img)
+    lbl_count_pred, polygon_img_pred = dict_to_polygons( segmentation_dict_pred, img)
+
+    # binarization matrix
+    bin_mask = cv2.threshold( cv2.cvtColor( np.array(img), cv2.COLOR_BGR2GRAY ), 0, 255, cv2.THRESH_OTSU )/255
+
+    # How many pixels in each polygon?
+    count_gt = collections.Counter( polygon_img_gt * bin_mask )
+    count_pred = collections.Counter( polygon_img_pred * bin_mask )
+
+    lbl_max = max(  lbl_count_gt, lbl_count_pred )
+    intersection_matrix = collections.Counter( (polygon_img_gt*bin_mask + (lbl_max+1)*polygon_img_pred*bin_mask).flatten().list() )
+
+    confusion_matrix = np.array((lbl_gt_count, lbl_pred_count))
+
+    for lbl_gt, lbl_pred in itertools.product( range(lbl_count_gt+1), range(lbl_count_pred+1 )):
+        intersection = intersection_matrix[ polygon_img_gt*bin_mask + (lbl_max+1)*polygon_img_pred*bin_mask ]
+        union = count_gt[ label_gt ] + count_pred[ label_pred ] - intersection
+        confusion_matrix[lbl_gt-1, lbl_pred-1]=intersection/union
+
+    return confusion_matrix
+
+
+def get_mask( img: np.ndarray ) -> np.ndarray:
+    """
+    Compute a binary mask from an image.
+
+    Args:
+        img (np.ndarray): input image 
+    """
+    if img.shape[2]>1:
+        img = cv2.cvtColor( img, cv2.COLOR_BGR2GRAY )
+
+    return cv2.threshold( img, 0, 255, cv2.THRESH_OTSU )[1] / 255
+
+
+
+def get_confusion_matrix_from_arrays( polygon_img_gt: np.ndarray, polygon_img_pred: np.ndarray, mask: np.ndarray, label_counts: Tuple[int,int] ) -> np.ndarray:
+    """
+    Compute a confusion matrix for segmentation output.
+
+    Args:
+        img (np.ndarray): input image (4-channel, unsigned 8-bit integers).
+        segmentation_dict_gt (np.ndarray): the GT dictionary describing the lines (see above)
+        segmentation_dict_pred (np.ndarray): the inferred dictionary describing the lines, as given by the Kraken library call
+
+    Output:
+        np.ndarray: a matrix containing I/U scores for all pairs (GT polygon pi, predicted polygon pj)
+
+    TODO
+    """
+
+    # 4-channel image to flat 32-bit image
+    print('polygon_img_gt.shape = {}, polygon_img_pred.shape = {}'.format( polygon_img_gt.shape, polygon_img_pred.shape ))
+    polygon_img_gt=rgba_uint8_to_int32( polygon_img_gt )
+    polygon_img_pred=rgba_uint8_to_int32( polygon_img_pred )
+    print('Collating bytes: polygon_img_gt.shape = {}, polygon_img_pred.shape = {}'.format( polygon_img_gt.shape, polygon_img_pred.shape ))
+
+    # introducing tensor type here because it is hashable and can be passed to Counter,
+    # but ultimately the rest of the code should use tensors too
+    polygon_img_gt = torch.from_numpy( polygon_img_gt )
+    polygon_img_pred = torch.from_numpy( polygon_img_pred )
+    mask = torch.from_numpy( mask )
+
+    # How many pixels in each polygon?
+    # Technical debt here: Counter cannot work on Numpy arrays, hence the messy writing
+    count_gt = collections.Counter( polygon_img_gt * mask )
+    count_pred = collections.Counter( polygon_img_pred * mask )
+
+    lbl_max = max( label_counts )
+    intersection_matrix = collections.Counter( list((polygon_img_gt*mask + (lbl_max+1)*polygon_img_pred*mask).flatten()) )
+
+    confusion_matrix = np.zeros((lbl_max, lbl_max))
+
+    for lbl_gt, lbl_pred in itertools.product( range(lbl_max+1), range(lbl_max+1 )):
+        intersection = intersection_matrix[ polygon_img_gt*mask + (lbl_max+1)*polygon_img_pred*mask ]
+        union = count_gt[ lbl_gt ] + count_pred[ lbl_pred ] - intersection
+        confusion_matrix[lbl_gt-1, lbl_pred-1] = intersection/union if union else 0
+
+    return confusion_matrix
+
+
+def evaluate( cm: np.ndarray ) -> np.ndarray:
+
+    label_count = cm.shape[0]
+    # correctly A-predictions over sum of all A predictions (=row sum)
+    precision = np.array( [ cm[l,l] / sum(cm[l,:]) for a in range( label_count ) ] )
+    # how much of all GT A-labels are correctly predicted (=col sum)
+    recall = np.array( [ cm[l,l] / sum(cm[l,:]) for a in range( label_count ) ] )
+
+    f_one = 2 * (precision+recall) / (precision*recall )
+
+    return f_one[ f1>.9 ] / label_count
+
+
+
+def dummy():
+    return True
+
+def dict_to_polygons( segmentation_dict: dict, img: Image.Image ) -> Tuple[int, np.ndarray]:
+    """
+        Args:
+            segmentation_dict (dict): kraken's segmentation output, i.e. a dicitonary of the form::
+
+                 {  'text_direction': '$dir',
+                 'type': 'baseline',
+                 'lines': [
+                   {'baseline': [[x0, y0], [x1, y1], ...], 'boundary': [[x0, y0], [x1, y1], ... [x_m, y_m]]},
+                   ...
+                 ]
+                 'regions': [ ... ] }
+            img (Image.Image): the input image
+
+        Output:
+            tuple: a pair with the maximum value of the line labels and the polygons rendered as a 4-channel image.
+
+            TODO: add shape into variable names and meaning of points
+
+            let's settle on input/ouput as CHW
+
+    """
+    polygon_boundaries = [ line['boundary'] for line in segmentation_dict['lines'] ]
+
+    # create 2D matrix of 32-bit integers 
+    # (fillPoly() only accepts signed integers - risk of overflow is non-existent)
+    polygon_img = np.zeros( tuple( reversed(img.size)), dtype=np.int32 )
+
+    # rendering polygons ( RGBA - 4 bytes - integer tensor out of it - pack label on the alpha channel - remembering that opencv is GBR)
+    for lbl, polyg in enumerate( polygon_boundaries ):
+        # Notes: 
+        # - fillPoly expects polygs as array of arrays as its second parameter
+        cv2.fillPoly( polygon_img, np.array( [polyg] ), lbl+1)
+
+    # 8-bit/pixel, 4 channels (note: order is little-endian)
+    polygon_img = array_to_rgba_uint8( polygon_img )
+
+    # max label + polygons as an image
+    return (len(polygon_boundaries)+1, polygon_img)
+
+
+def array_to_rgba_uint8( arr: np.ndarray ) -> np.ndarray:
+    return arr.astype( np.uint32 ).view( np.uint8 ).reshape( arr.shape + (4,) )
+
+
+def rgba_uint8_to_int32( arr: np.ndarray ) -> np.ndarray:
+    return arr.view( np.int32 ).reshape( arr.shape[:-1] )
---
+++ apps/ddpa_lines/tests/test_ddpa_line_detect.py
@@ -0,0 +1,88 @@
+#!/usr/env python3
+
+import unittest
+from torch import Tensor
+from pathlib import Path
+#from apps.ddpa_lines import seglib
+import logging
+import pytest
+from PIL import Image
+from torch import Tensor
+import json
+import numpy as np
+
+import sys
+
+# Append app's root directory to the Python search path
+sys.path.append( str( Path(__file__).parents[1] ) )
+
+import seglib
+
+class LineDetectTest( unittest.TestCase ):
+
+    @classmethod
+    def setUpClass(self):
+        self.data_path = Path( __file__ ).parent.joinpath('data')
+
+    # testing local imports
+    def test_dummy_1(self):
+        self.assertTrue( seglib.dummy() )
+
+    def Dtest_line_segmentation_model_not_found( self ):
+        model = Path('nowhere_to_be_found.mlmodel')
+        input_image = self.data_path.joinpath('NA-ACK_14201223_01485_r-r1.png')
+        with pytest.raises( FileNotFoundError ) as e:
+            seglib.line_segment(input_image, model)
+
+    def Dtest_line_segmentation_output( self ):
+        model = self.data_path.joinpath('kraken_default_blla.mlmodel')
+        with Image.open( self.data_path.joinpath('NA-ACK_14201223_01485_r-r1.png')) as input_image:
+            lbl, polygons = seglib.line_segment( input_image, model )
+            self.assertTrue( type(lbl) is int and typea(polygons) is np.ndarray )
+
+    def test_segmentation_dict_to_polygons_label_count( self ):
+        with open( self.data_path.joinpath('segdict_NA-ACK_14201223_01485_r-r1+model_20.json'), 'r') as segdict_file, Image.open( self.data_path.joinpath('NA-ACK_14201223_01485_r-r1.png'), 'r') as input_image:
+            segdict = json.load( segdict_file )
+            lbl, polygons = seglib.dict_to_polygons( segdict, input_image )
+            self.assertTrue( type(lbl) is int and type(polygons) is np.ndarray )
+
+    def test_segmentation_dict_to_polygons_polygon_img_type( self ):
+        with open( self.data_path.joinpath('segdict_NA-ACK_14201223_01485_r-r1+model_20.json'), 'r') as segdict_file, Image.open( self.data_path.joinpath('NA-ACK_14201223_01485_r-r1.png'), 'r') as input_image:
+            segdict = json.load( segdict_file )
+            lbl, polygons = seglib.dict_to_polygons( segdict, input_image )
+            # output should be a 4-channel tensor of 8-bit unsigned integers
+            self.assertTrue( polygons.dtype is np.dtype('uint8') )
+            self.assertEqual( polygons.shape, tuple(reversed(input_image.size)) + (4,))
+
+
+    def test_line_segmentation_confusion_matrix( self ):
+
+        input_img = Image.open( self.data_path.joinpath('NA-ACK_14201223_01485_r-r1.png'))
+        print('input_img.size = {} (PIL.Image - WH)'.format( input_img.size ))
+
+        input_img_arr = np.array( input_img )
+        print('input_img_arr.shape = {} (np.ndarray - HW)'.format( input_img_arr.shape ))
+
+        dict_pred = json.load( open(self.data_path.joinpath('segdict_NA-ACK_14201223_01485_r-r1+model_20.json'), 'r'))
+        # (use a MonasteriumTeklia DS class method to generate a json dictionary)
+        dict_gt = json.load( open(self.data_path.joinpath('NA-ACK_14201223_01485_r-r1.json'), 'r'))
+
+        # This is confusing
+
+        # input is a PIL Image
+        label_count_gt, polygon_gt = seglib.dict_to_polygons( dict_gt, input_img )
+        label_count_pred, polygon_pred = seglib.dict_to_polygons( dict_pred, input_img )
+
+        # input is a Numpy array
+        mask = seglib.get_mask( input_img_arr )
+        print('mask.shape = {} (np.ndarray - HW)'.format( mask.shape ))
+
+        confusion_matrix = seglib.get_confusion_matrix_from_arrays(polygon_gt, polygon_pred, mask, (label_count_gt, label_count_pred) )
+
+
+        self.assertTrue( type(confusion_matrix) is np.ndarray )
+
+
+
+if __name__ == "__main__":
+    unittest.main()
