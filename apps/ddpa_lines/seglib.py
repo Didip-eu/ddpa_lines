c85f3da2f665594b5ad0c6acc573f89c1287cf74
Nicolas Renet <nprenet@gmail.com>
2024-05-10 14:42:46 +0200

    Bug fixes and clean-up, before overhaul.

--- apps/ddpa_lines/seglib.py
+++ apps/ddpa_lines/seglib.py
@@ -1,6 +1,6 @@
 from kraken import blla
 from kraken.lib import vgsl
-from PIL import Image
+from PIL import Image, ImageDraw
 from pathlib import Path
 import torchvision
 
@@ -11,13 +11,20 @@ from torchvision.transforms import v2
 from torch import Tensor
 
 import numpy as np
-import cv2
 import collections
-from typing import Tuple
+from typing import Tuple, Callable
 import itertools
 import torch
 
+import skimage as ski
+from matplotlib import pyplot as plt
 
+
+
+RED = (255,0,0)
+GREEN = (0,255,0)
+YELLOW = (0,255,255)
+BLUE = (0,0,255)
 
 def line_segment(img: Image.Image, model_path: str):
     r"""
@@ -32,27 +39,23 @@ def line_segment(img: Image.Image, model_path: str):
     if not Path( model_path ).exists():
         raise FileNotFoundError("Cound not find model file", model_path)
     # you want to load the model once (loaded model as a parameter)
-    return dict_to_polygons( blla.segment( img, model=vgsl.TorchVGSLModel.load_model( model_path )), img )
+    return dict_to_polygon_map( blla.segment( img, model=vgsl.TorchVGSLModel.load_model( model_path )), img )
 
 
-# loading data and calling thisn one
-
-
-def get_mask( img_wh: Image.Image ) -> torch.Tensor:
+def get_mask( img_whc: Image.Image, thresholding_alg: Callable=ski.filters.threshold_otsu ) -> torch.Tensor:
     """
-    Compute a binary mask from an image.
+    Compute a binary mask from an image, using the given thresholding algorithm: FG=1s, BG=0s
 
     Args:
-        img (torch.Tensor): input image 
+        img (PIL image): input image
+    Output:
+        torch.Tensor: a binary map with FG pixels=1 and BG=0.
     """
-    img_hw = np.array( img_wh )
-    if img_hw.shape[2]>1:
-        img_hw = cv2.cvtColor( img_hw, cv2.COLOR_BGR2GRAY )
+    img_hwc= np.array( img_whc )
+    threshold = thresholding_alg( ski.color.rgb2gray( img_hwc) if img_hwc.shape[2]>1 else img_hwc )*255
+    img_bin_hw = torch.from_numpy( (img_hwc < threshold)[:,:,0] ) 
 
-    img_bin_hw = torch.from_numpy( cv2.threshold( img_hw, 0, 255, cv2.THRESH_OTSU )[1] / 255 )
-
-    return img_bin_hw 
-
+    return img_bin_hw.int()
 
 
 def get_confusion_matrix_from_arrays( polygon_img_gt: torch.Tensor, polygon_img_pred: torch.Tensor, mask: torch.Tensor, label_counts: Tuple[int,int] ) -> torch.Tensor:
@@ -60,41 +63,48 @@ def get_confusion_matrix_from_arrays( polygon_img_gt: torch.Tensor, polygon_img_
     Compute a confusion matrix for segmentation output.
 
     Args:
-        img (np.ndarray): input image (4-channel, unsigned 8-bit integers).
-        segmentation_dict_gt (np.ndarray): the GT dictionary describing the lines (see above)
-        segmentation_dict_pred (np.ndarray): the inferred dictionary describing the lines, as given by the Kraken library call
+        polygon_img_gt (torch.Tensor): ground truth polygon map (4-channel, unsigned 8-bit integers).
+        polygon_img_pred (torch.Tensor): predicted polygon map (4-channel, unsigned 8-bit integers).
+        mask (torch.Tensor): binary mask representing the foreground pixels
+        label_counts (tuple): number of labels in each map
 
     Output:
-        np.ndarray: a matrix containing I/U scores for all pairs (GT polygon pi, predicted polygon pj)
+        torch.Tensor: a matrix containing I/U scores for all pairs (GT polygon pi, predicted polygon pj)
 
-    TODO
     """
 
     # 4-channel image to flat 32-bit image
     print('polygon_img_gt.shape = {}, polygon_img_pred.shape = {}'.format( polygon_img_gt.shape, polygon_img_pred.shape ))
     polygon_img_gt=rgba_uint8_to_int32( polygon_img_gt )
+    polygon_gt_fg = polygon_img_gt * mask
     polygon_img_pred=rgba_uint8_to_int32( polygon_img_pred )
+    polygon_pred_fg = polygon_img_pred * mask
+
     print('Collating bytes: polygon_img_gt.shape = {}, polygon_img_pred.shape = {}'.format( polygon_img_gt.shape, polygon_img_pred.shape ))
 
-    # introducing tensor type here because it is hashable and can be passed to Counter,
-    # but ultimately the rest of the code should use tensors too
-    polygon_img_gt = polygon_img_gt
-    polygon_img_pred = polygon_img_pred
-
     # How many pixels in each polygon?
-    # Technical debt here: Counter cannot work on Numpy arrays, hence the messy writing
-    count_gt = collections.Counter( polygon_img_gt * mask )
-    count_pred = collections.Counter( polygon_img_pred * mask )
+    count_gt = collections.Counter( polygon_gt_fg.flatten().tolist() )
+    count_pred = collections.Counter(polygon_pred_fg.flatten().tolist() )
 
     lbl_max = max( label_counts )
-    intersection_matrix = collections.Counter( list((polygon_img_gt*mask + (lbl_max+1)*polygon_img_pred*mask).flatten()) )
+
+    # For each position w/ to (possibly different) labels m and n,
+    # compute a unique integer ( m + lmax*n)
+    # Eg. 
+    #    l   l_hat
+    #    4 + 4*7 →  32
+    #    5 + 4*7 →  33
+    intersection_matrix = polygon_gt_fg + (lbl_max+1)*polygon_pred_fg
+    intersection_counts = collections.Counter( intersection_matrix.flatten().tolist())
 
     confusion_matrix = torch.zeros((lbl_max, lbl_max))
 
-    for lbl_gt, lbl_pred in itertools.product( range(lbl_max+1), range(lbl_max+1 )):
-        intersection = intersection_matrix[ polygon_img_gt*mask + (lbl_max+1)*polygon_img_pred*mask ]
-        union = count_gt[ lbl_gt ] + count_pred[ lbl_pred ] - intersection
-        confusion_matrix[lbl_gt-1, lbl_pred-1] = intersection/union if union else 0
+    for lbl_gt, lbl_pred in itertools.product( range(1,lbl_max+1), range(1,lbl_max+1)):
+        intersect_count_key = lbl_gt + (lbl_max+1)*lbl_pred
+        intersect_count = intersection_counts[ intersect_count_key ] if intersect_count_key else 0
+        print( lbl_gt, '∩', lbl_pred, "=", intersect_count)
+        union_count = count_gt[ lbl_gt ] + count_pred[ lbl_pred ] - intersect_count
+        confusion_matrix[lbl_gt-1, lbl_pred-1] = intersect_count/union_count if union_count else 0
 
     return confusion_matrix
 
@@ -112,31 +122,24 @@ def evaluate( cm: np.ndarray ) -> float:
     return f_one[ f1>.9 ] / label_count
 
 
-
-def dummy():
-    return True
-
-def dict_to_polygons( segmentation_dict: dict, img: Image.Image ) -> Tuple[int, torch.Tensor]:
+def dict_to_polygon_map( segmentation_dict: dict, img: Image.Image ) -> Tuple[int, torch.Tensor]:
     """
-        Args:
-            segmentation_dict (dict): kraken's segmentation output, i.e. a dicitonary of the form::
+    Store line polygons into a tensor, as pixel maps.
 
-                 {  'text_direction': '$dir',
-                 'type': 'baseline',
-                 'lines': [
-                   {'baseline': [[x0, y0], [x1, y1], ...], 'boundary': [[x0, y0], [x1, y1], ... [x_m, y_m]]},
-                   ...
-                 ]
-                 'regions': [ ... ] }
-            img (Image.Image): the input image
+    Args:
+        segmentation_dict (dict): kraken's segmentation output, i.e. a dicitonary of the form::
 
-        Output:
+             {  'text_direction': '$dir',
+             'type': 'baseline',
+             'lines': [
+               {'baseline': [[x0, y0], [x1, y1], ...], 'boundary': [[x0, y0], [x1, y1], ... [x_m, y_m]]},
+               ...
+             ]
+             'regions': [ ... ] }
+        img (Image.Image): the input image
+
+    Output:
             tuple: a pair with the maximum value of the line labels and the polygons rendered as a 4-channel image (a tensor).
-
-            TODO: add shape into variable names and meaning of points
-
-            let's settle on input/ouput as CHW
-
     """
     polygon_boundaries = [ line['boundary'] for line in segmentation_dict['lines'] ]
 
@@ -144,17 +147,74 @@ def dict_to_polygons( segmentation_dict: dict, img: Image.Image ) -> Tuple[int, 
     # (fillPoly() only accepts signed integers - risk of overflow is non-existent)
     polygon_img = np.zeros( tuple( reversed(img.size)), dtype=np.int32 )
 
-    # rendering polygons ( RGBA - 4 bytes - integer tensor out of it - pack label on the alpha channel - remembering that opencv is GBR)
+    # rendering polygons (RGBA)
+    label = 0
     for lbl, polyg in enumerate( polygon_boundaries ):
-        # Notes: 
-        # - fillPoly expects polygs as array of arrays as its second parameter
-        cv2.fillPoly( polygon_img, np.array( [polyg] ), lbl+1)
+        #cv2.fillPoly( polygon_img, np.array( [polyg] ), lbl+1)
+        # Notes:
+        # - with this method, a polygon intersection has its own
+        #   (possibly non-unique) label
+        label = lbl + 1
+        polygon_img += ( ski.draw.polygon2mask( img.size[::-1], polyg ) * label )
+
+        # Another choice is to override an existing label with the new one:
+        # - intersections between polygons are lost
+        #polygon_img = ( ski.draw.polygon2mask( img.size[::-1], polyg ) * label )
+
+    print("Max polygon = ", label )
+    print("Max label = ", polygon_img.max())
+     
+    print("polygon_boundaries.length =", len(polygon_boundaries))
+    #ski.io.imshow( polygon_img.transpose() )
+    #plt.show()
+
 
     # 8-bit/pixel, 4 channels (note: order is little-endian)
     polygon_img = array_to_rgba_uint8( torch.from_numpy( polygon_img ))
+    #ski.io.imshow( polygon_img.permute(1,2,0).numpy() )
+    #plt.show()
 
     # max label + polygons as an image
-    return (len(polygon_boundaries)+1,  polygon_img )
+    return (label,  polygon_img )
+
+def dict_to_polygon_lines( segmentation_dict: dict, img_file: Path , output_dir, factor=.5 ):
+    """
+    Visualize polygons on the input image.
+
+    Args:
+        segmentation_dict (dict): kraken's segmentation output, i.e. a dicitonary of the form::
+
+             {  'text_direction': '$dir',
+             'type': 'baseline',
+             'lines': [
+               {'baseline': [[x0, y0], [x1, y1], ...], 'boundary': [[x0, y0], [x1, y1], ... [x_m, y_m]]},
+               ...
+             ]
+             'regions': [ ... ] }
+        img (Path): input image's path.
+
+    Output:
+        None (saves a PNG image) 
+    """
+
+    with Image.open( img_file) as img:
+
+        polygon_boundaries = [ line['boundary'] for line in segmentation_dict['lines'] ]
+
+        draw = ImageDraw.Draw( img )
+
+        # rendering polygons (RGBA)
+        label = 0
+        for lbl, polyg in enumerate( polygon_boundaries ):
+            label = lbl + 1
+            color = (255,0,0) if lbl%2 else (0,0,255)
+            draw.polygon( [ tuple(pt) for pt in polyg ], outline=color, width=4)
+
+        img.resize( tuple( int(elt*factor) for elt in img.size ))
+        output_file = Path(output_dir, img_file.with_suffix('.polygons.png') )
+        print(output_file)
+        img.save(output_file, format="PNG")
+
 
 
 def array_to_rgba_uint8( img_hw: torch.Tensor ) -> torch.Tensor:
@@ -165,3 +225,113 @@ def array_to_rgba_uint8( img_hw: torch.Tensor ) -> torch.Tensor:
 def rgba_uint8_to_int32( img_chw: torch.Tensor ) -> torch.Tensor:
     img_hw = img_chw.permute(1,2,0).view( torch.int32 ).reshape( img_chw.shape[1:] )
     return img_hw
+
+
+def polygon_set_display( input_img: Image.Image, polygons: torch.Tensor ) -> np.ndarray:
+    """
+    Render a single set of polygons using two colors (alternate between odd- and even-numbered lines).
+
+    Args:
+        input_img (Image.Image): the original manuscript image, as opened with PIL.
+        polygons (torch.Tensor): polygon set, encoded as a 4-channel, 8-bit tensor.
+    Output:
+        np.ndarray: A BGR image (3 channels, 8-bit unsigned integers).
+    """
+
+    input_img = np.asarray( input_img )
+
+    # create mask tensor for all pred. polygon: odd-numbered polygons in R, even-numbered ones in G
+    polygon_32b_img = rgba_uint8_to_int32( polygons ) # 4-channel -> 1-channel
+    odd_polygon_8b1c_mask, even_polygon_8b1c_mask = [ torch.logical_and( (polygon_32b_img > 0 ), pt).numpy().astype( np.uint8 ) for pt in ((polygon_32b_img % 2), (polygon_32b_img % 2) == 0) ]
+    odd_polygon_8b3c_mask, even_polygon_8b3c_mask = [ np.stack((pm, pm, pm), axis=2) for pm in (odd_polygon_8b1c_mask, even_polygon_8b1c_mask) ]
+    
+    # foreground color image (red or green, with RGB), masked
+    full_red, full_green = [ np.full( input_img.shape, color, dtype=np.uint8) for color in (RED, GREEN) ]
+    #print(foreground_red.dtype, foreground_green.dtype, odd_polygon_8b3c_mask.dtype)
+    fg_red_masked, fg_green_masked = [ fg * mask  for (fg, mask) in ((full_red, odd_polygon_8b3c_mask), (full_green, even_polygon_8b3c_mask)) ]
+
+    # combine both FG layers
+    foreground = fg_red_masked + fg_green_masked 
+
+    # BG + FG
+    alpha = .75
+    return (input_img * alpha + foreground * (1-alpha)).astype('uint8')
+
+def polygon_two_set_display( input_img: Image.Image, polygons1: torch.Tensor, polygons2: torch.Tensor ) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Render two sets of polygons (typically: GT and pred.) using two colors, for human diagnosis. For clarity's sake,
+    it returns 2 images:
+
+    + one highlights the even-numbered lines, showing both sets of polygons (1: red, 2: green)
+    + one highlights the odd-numbered lines, 
+
+    Args:
+        input_img (Image.Image): the original manuscript image, as opened with PIL.
+        polygons1 (torch.Tensor): polygon set #1, encoded as a 4-channel, 8-bit tensor.
+        polygons2 (torch.Tensor): polygon set #2, encoded as a 4-channel, 8-bit tensor.
+    Output:
+        A tuple containing two BGR images (8-bit unsigned integers), rendering the even-numbered lines
+        and the odd-numbered lines, respectively.
+    """
+    input_img = np.asarray( input_img )
+
+    # create mask tensor for polygon set #1
+    polygon_32b_img_1, polygon_32b_img_2 = [ rgba_uint8_to_int32( p ) for p in (polygons1, polygons2) ]  # 4-channel -> 1-channel
+
+    # sets 1 and 2, even-numbered lines
+    mask1, mask2 = [ torch.logical_and( (p % 2), ( p > 0 )).numpy().astype( np.uint8 ) for p in (polygon_32b_img_1, polygon_32b_img_2) ]
+    # sets 1 and 2, odd-numbered lines
+    mask3, mask4 = [ torch.logical_and( (p % 2) == 0, ( p > 0 )).numpy().astype( np.uint8 ) for p in (polygon_32b_img_1, polygon_32b_img_2) ]
+    pg_8b3c_mask_1, pg_8b3c_mask_2, pg_8b3c_mask_3, pg_8b3c_mask_4 = [ np.stack((p, p, p), axis=2) for p in (mask1, mask2,  mask3, mask4) ]
+
+    full_red, full_green = [ np.full( input_img.shape, color, dtype=np.uint8 ) for color in (RED, GREEN) ]
+    fg_red_masked_even, fg_green_masked_even, fg_red_masked_odd, fg_green_masked_odd = [ (fg * mask ) for (fg, mask) in ((full_red, pg_8b3c_mask_1), (full_green, pg_8b3c_mask_2), (full_red, pg_8b3c_mask_3), (full_green, pg_8b3c_mask_4)) ]
+
+    # combine both FG layers
+    foreground_even = fg_red_masked_even + fg_green_masked_even
+    foreground_odd = fg_red_masked_odd + fg_green_masked_odd
+
+    # BG + FG
+    alpha = .75
+    oimg1, oimg2 = [ (input_img * alpha + fg * (1-alpha)).astype('uint8') for fg in (foreground_even, foreground_odd) ]
+    return ( oimg1, oimg2 ) 
+
+
+def Dpolygon_two_set_display_alt( input_img: Image.Image, polygons1: torch.Tensor, polygons2: torch.Tensor ) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Render two sets of polygons (typically: GT and pred.) using two colors, for human diagnosis. 
+    Even- and odd-numbered lines use different pairs of colors.
+
+    Note: output is utterly confusing.
+
+
+    Args:
+        input_img (Image.Image): the original manuscript image, as opened with PIL.
+        polygons1 (torch.Tensor): polygon set #1, encoded as a 4-channel, 8-bit tensor.
+        polygons2 (torch.Tensor): polygon set #2, encoded as a 4-channel, 8-bit tensor.
+    Output:
+        np.ndarray: A BGR image (3 channels, 8-bit unsigned integers).
+    """
+    # do everything in openCV
+    input_img = cv2.cvtColor( np.array( input_img ), cv2.COLOR_RGB2BGR )
+
+    polygon_32b_img_1, polygon_32b_img_2 = [ rgba_uint8_to_int32( p ) for p in (polygons1, polygons2) ]  # 4-channel -> 1-channel
+    # sets 1 and 2, even-numbered lines
+    mask1, mask2 = [ torch.logical_and( (p % 2), ( p > 0 )).numpy().astype( np.uint8 ) for p in (polygon_32b_img_1, polygon_32b_img_2) ]
+    # sets 1 and 2, odd-numbered lines
+    mask3, mask4 = [ torch.logical_and( (p % 2) == 0, ( p > 0 )).numpy().astype( np.uint8 ) for p in (polygon_32b_img_1, polygon_32b_img_2) ]
+    pg_8b3c_mask_1, pg_8b3c_mask_2, pg_8b3c_mask_3, pg_8b3c_mask_4 = [ cv2.merge((p, p, p)) for p in (mask1, mask2,  mask3, mask4) ]
+
+    # foreground color image: red/green (set 1), yellow/blue (set 2) 
+    full_red, full_green, full_yellow, full_blue = [ np.full( input_img.shape, color, dtype=np.uint8 ) for color in (RED, GREEN, YELLOW, BLUE) ]
+    fg_red_masked, fg_green_masked, fg_yellow_masked, fg_blue_masked = [ cv2.multiply( f, m ) for (f, m) in ((full_red, pg_8b3c_mask_1), (full_green, pg_8b3c_mask_3), (full_yellow, pg_8b3c_mask_2), (full_blue, pg_8b3c_mask_4)) ]
+
+    # combine 4 FG layers
+    foreground = cv2.add( cv2.add( cv2.add( fg_red_masked, fg_green_masked), fg_yellow_masked), fg_blue_masked)
+
+    # BG + FG
+    return cv2.addWeighted( input_img, .75, foreground, .25, 0)
+
+
+def dummy():
+    return True
--- apps/ddpa_lines/tests/test_ddpa_line_detect.py
+++ apps/ddpa_lines/tests/test_ddpa_line_detect.py
@@ -11,6 +11,8 @@ from torch import Tensor
 import json
 import numpy as np
 import torch
+import skimage as ski
+from matplotlib import pyplot as plt
 
 import sys
 
@@ -19,70 +21,128 @@ sys.path.append( str( Path(__file__).parents[1] ) )
 
 import seglib
 
+
+# make import conditional for visualization functions
+
+
 class LineDetectTest( unittest.TestCase ):
 
     @classmethod
     def setUpClass(self):
         self.data_path = Path( __file__ ).parent.joinpath('data')
 
-    # testing local imports
     def test_dummy_1(self):
+        """
+        A dummy test, as a sanity check for the test framework.
+        """
         self.assertTrue( seglib.dummy() )
 
-    def Dtest_line_segmentation_model_not_found( self ):
+    def test_line_segmentation_model_not_found( self ):
+        """
+        An exception should be raised when no segmentation model found.
+        """
         model = Path('nowhere_to_be_found.mlmodel')
         input_image = self.data_path.joinpath('NA-ACK_14201223_01485_r-r1.png')
         with pytest.raises( FileNotFoundError ) as e:
             seglib.line_segment(input_image, model)
 
+
+    def Dtest_flat_to_cube( self ):
+        """
+        To be removed (utility function).
+        """
+        t = torch.randint(120398, (7,5), dtype=torch.int32 )
+        self.assertTrue( torch.all( t.eq( seglib.rgba_uint8_to_int32( seglib.array_to_rgba_uint8(t) ))))
+
+    def test_binary_mask_from_image_real_img(self):
+        """
+        Binarization function should return a Boolean tensor with shape( h,w )
+        """
+        input_img = Image.open( self.data_path.joinpath('NA-ACK_14201223_01485_r-r1_reduced.png'))
+        mask = seglib.get_mask( input_img )
+        self.assertEqual( type(mask), torch.Tensor ) and self.assertEqual( mask.dtype, torch.int32 )
+        self.assertEqual( mask.shape, tuple(reversed(input_img.size)))
+
+    def test_binary_mask_from_image_fg_bg(self):
+        """
+        Binary map should be 1 for FG pixels, 0 otherwise.
+        """
+        img_arr = np.full((15,15,3), 200, dtype=np.uint8 ) # background
+        img_arr[8,8]=5 # foreground = 1 pixel
+        binary_map = seglib.get_mask( Image.fromarray( img_arr) )
+        self.assertEqual( binary_map[8,8], 1) # single FG pixel = T
+        self.assertEqual( torch.sum(binary_map).item(), 1 ) # remaining pixels = F
+
     def Dtest_line_segmentation_output( self ):
+        """
+        seglib.line_segment( image, model ) --a thin wrapper around Kraken's blla.segment-- should return a tuple (labels, polygons).
+        """
         model = self.data_path.joinpath('kraken_default_blla.mlmodel')
         with Image.open( self.data_path.joinpath('NA-ACK_14201223_01485_r-r1.png')) as input_image:
             lbl, polygons = seglib.line_segment( input_image, model )
-            self.assertTrue( type(lbl) is int and typea(polygons) is np.ndarray )
-
-    def test_segmentation_dict_to_polygons_label_count( self ):
-        with open( self.data_path.joinpath('segdict_NA-ACK_14201223_01485_r-r1+model_20.json'), 'r') as segdict_file, Image.open( self.data_path.joinpath('NA-ACK_14201223_01485_r-r1.png'), 'r') as input_image:
-            segdict = json.load( segdict_file )
-            lbl, polygons = seglib.dict_to_polygons( segdict, input_image )
             self.assertTrue( type(lbl) is int and type(polygons) is torch.Tensor )
 
-    def test_segmentation_dict_to_polygons_polygon_img_type( self ):
-        with open( self.data_path.joinpath('segdict_NA-ACK_14201223_01485_r-r1+model_20.json'), 'r') as segdict_file, Image.open( self.data_path.joinpath('NA-ACK_14201223_01485_r-r1.png'), 'r') as input_image:
+    def Dtest_segmentation_dict_to_polygon_map_label_count( self ):
+        """
+        seglib.dict_to_polygon_map(dict, image) should return a tuple (labels, polygons) 
+        """
+        with open( self.data_path.joinpath('segdict_NA-ACK_14201223_01485_r-r1+model_20_reduced.json'), 'r') as segdict_file, Image.open( self.data_path.joinpath('NA-ACK_14201223_01485_r-r1_reduced.png'), 'r') as input_image:
             segdict = json.load( segdict_file )
-            lbl, polygons = seglib.dict_to_polygons( segdict, input_image )
+            lbl, polygons = seglib.dict_to_polygon_map( segdict, input_image )
+            self.assertTrue( type(lbl) is int and type(polygons) is torch.Tensor )
+
+
+    def Dtest_segmentation_dict_to_polygon_map_polygon_img_type( self ):
+        with open( self.data_path.joinpath('segdict_NA-ACK_14201223_01485_r-r1+model_20_reduced.json'), 'r') as segdict_file, Image.open( self.data_path.joinpath('NA-ACK_14201223_01485_r-r1_reduced.png'), 'r') as input_image:
+            segdict = json.load( segdict_file )
+            lbl, polygons = seglib.dict_to_polygon_map( segdict, input_image )
             # output should be a 4-channel tensor of 8-bit unsigned integers
             # self.assertTrue( polygons.dtype is np.dtype('uint8') )
             self.assertEqual( polygons.shape, (4,) + tuple(reversed(input_image.size)) )
 
-    def test_binary_mask_from_image(self):
-        input_img = Image.open( self.data_path.joinpath('NA-ACK_14201223_01485_r-r1.png'))
-        self.assertTrue( type(seglib.get_mask( input_img )), torch.BoolTensor )
-        self.assertEqual( seglib.get_mask( input_img ).shape, tuple(reversed(input_img.size)))
+    def Dtest_segmentation_dict_to_polygons_lines( self ):
+        """ 
+        To be removed (visualization code)
+        """
+        with open( self.data_path.joinpath('segdict_NA-ACK_14201223_01485_r-r1+model_20.json'), 'r') as segdict_file, Image.open( self.data_path.joinpath('NA-ACK_14201223_01485_r-r1_reduced.png'), 'r') as input_image:
+            segdict = json.load( segdict_file )
+            lbl, polygons = seglib.dict_to_polygon_lines( segdict, input_image )
+            self.assertTrue( True )
+
 
     def test_line_segmentation_confusion_matrix( self ):
+        """
+        This test takes way too long to complete and should ultimately be removed.
+        """
 
-        input_img = Image.open( self.data_path.joinpath('NA-ACK_14201223_01485_r-r1.png'))
-        print('input_img.size = {} (PIL.Image - WH)'.format( input_img.size ))
+        print('Dtest_line_segmentation_confusion_matrix()')
+        input_img = Image.open( self.data_path.joinpath('NA-ACK_14201223_01485_r-r1_reduced.png'))
+        #print('input_img.size = {} (PIL.Image - WH)'.format( input_img.size ))
 
-        dict_pred = json.load( open(self.data_path.joinpath('segdict_NA-ACK_14201223_01485_r-r1+model_20.json'), 'r'))
-        # (use a MonasteriumTeklia DS class method to generate a json dictionary)
-        dict_gt = json.load( open(self.data_path.joinpath('NA-ACK_14201223_01485_r-r1.json'), 'r'))
+        dict_pred = json.load( open(self.data_path.joinpath('segdict_NA-ACK_14201223_01485_r-r1+model_20_reduced.json'), 'r'))
+        # (note: used a MonasteriumTeklia DS class method to generate a json dictionary)
+        dict_gt = json.load( open(self.data_path.joinpath('NA-ACK_14201223_01485_r-r1_reduced.json'), 'r'))
 
 
         # input is a PIL Image, outputs are tensors
-        label_count_gt, polygon_gt = seglib.dict_to_polygons( dict_gt, input_img )
-        label_count_pred, polygon_pred = seglib.dict_to_polygons( dict_pred, input_img )
+        label_count_gt, polygon_gt = seglib.dict_to_polygon_map( dict_gt, input_img )
+        label_count_pred, polygon_pred = seglib.dict_to_polygon_map( dict_pred, input_img )
         mask = seglib.get_mask( input_img )
 
-        print('mask.shape = {} (np.ndarray - HW)'.format( mask.shape ))
+        print('mask.shape = {} (np.ndarray - HW)'.format( mask.shape ), "label_max=", max(label_count_gt, label_count_pred))
 
         confusion_matrix = seglib.get_confusion_matrix_from_arrays(polygon_gt, polygon_pred, mask, (label_count_gt, label_count_pred) )
+        torch.save( confusion_matrix, self.data_path.joinpath('confusion_matrix.pt') )
+
+        print(confusion_matrix)
 
         self.assertTrue( type(confusion_matrix) is torch.Tensor )
 
-    def Dtest_visualize( self ):
-        polygon_display = seglib.polygon_display( input_img, polygon_gt, polygon_pred )
+
+    def Dtest_evaluate( self ):
+        pass
+
+
 
 
 if __name__ == "__main__":
